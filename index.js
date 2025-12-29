5bcbccb67929856b46035335cd2eca0662913dc1
samuelalbuquerque34100-bit <samuelalbuquerque34100@gmail.com>
2025-12-29 07:53:22 -0300

    Update index.js
    bot base

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-index.js
-console.log("Bot iniciado");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Bot ligado como ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "!ping") {
+    message.reply("pong 🏓");
+  }
+});
+
+client.login(process.env.TOKEN);
